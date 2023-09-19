4825f2760fcef1a23af49d9d8df60851d657dee4
Princewill Udoh <udohprincewill144@gmail.com>
2023-09-19 03:02:30 +0100

    push

---
+++ javascript_advanced/5-mode.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>you are browsing as a guest</h1>
+        <p>pages you view in this window wont appear in the browser and they wont leave other traces, like cookies on the computer after you close all open guest windows. all files you download will be preserved, however.</p>
+        <button>learn more</button>
+    </div>
+    <p>Welcome Holberton!</p>
+    <button id="spookyButton">Spooky</button>
+    <button id="darkModeButton">Dark mode</button>
+    <button id="screamModeButton">Scream mode</button>
+    <script src="5-mode.js"></script>
+</body>
+</html>
