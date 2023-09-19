32160fa4bffd37c7c81f9064724a44a8f8b36bcc
Princewill Udoh <udohprincewill144@gmail.com>
2023-09-19 03:03:26 +0100

    push

--- javascript_advanced/5-mode.js
+++ javascript_advanced/5-mode.js
@@ -1,21 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-    <div>
-        <h1>you are browsing as a guest</h1>
-        <p>pages you view in this window wont appear in the browser and they wont leave other traces, like cookies on the computer after you close all open guest windows. all files you download will be preserved, however.</p>
-        <button>learn more</button>
-    </div>
-    <p>Welcome Holberton!</p>
-    <button id="spookyButton">Spooky</button>
-    <button id="darkModeButton">Dark mode</button>
-    <button id="screamModeButton">Scream mode</button>
-    <script src="5-mode.js"></script>
-</body>
-</html>
+function changeMode(size, weight, transform, background, color){
+    return function(){
+        document.body.style.fontSize = size + 'px';
+        document.body.style.fontWeight = weight;
+        document.body.style.textTransform = transform;
+        document.body.style.backgroundColor = background;
+        document.body.style.color = color;
+    }
+}
+function main(){
+    const spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+    const darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
+    const screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
+
+    document.getElementById('spookyButton').addEventListener('click', spooky);
+    document.getElementById('darkModeButton').addEventListener('click', darkMode);
+    document.getElementById('screamModeButton').addEventListener('click', screamMode);
+}
+main();
