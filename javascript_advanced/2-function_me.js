210f848da8e3d0b084482346f5c1952a0a51d3f9
Princewill Udoh <udohprincewill144@gmail.com>
2023-09-19 02:56:44 +0100

    push

---
+++ javascript_advanced/2-function_me.js
@@ -0,0 +1,6 @@
+function welcomeMessage(fullName){
+    alert(`Welcome ${fullName}`);
+};
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred')
