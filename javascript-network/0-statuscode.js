945427a2d28ae0e5645b1ee8c1a5996870476cf6
Princewill Udoh <udohprincewill144@gmail.com>
2023-09-19 02:19:20 +0100

    push

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+    if (error){
+        console.error(error.message);
+    }else {
+        console.log(`code: ${response.statusCode}`);
+    }
+});
+    
